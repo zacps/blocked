4315921fd913e3fa785bcab9cbc2617303966df0
Zac Pullar-Strecker <zacmps@gmail.com>
2020-05-29 18:23:56 +1200

    Improvements & cargo fmt

--- blocked/src/lib.rs
+++ blocked/src/lib.rs
@@ -2,23 +2,26 @@
 
 extern crate proc_macro;
 
-use proc_macro::{Diagnostic, Level, TokenStream, Span};
-use syn::{parse::Parser, LitStr, punctuated::Punctuated, Token};
+use proc_macro::{Diagnostic, Level, Span, TokenStream};
+use syn::{parse::Parser, punctuated::Punctuated, LitStr, Token};
 
+use git2::Repository;
+use lazy_static::lazy_static;
+use regex::Regex;
 use reqwest::header::{self, HeaderMap};
 use serde::Deserialize;
 use url::Url;
-use regex::Regex;
-use lazy_static::lazy_static;
-use git2::Repository;
 
 lazy_static! {
-    static ref ISSUE: Regex = Regex::new(r"#?\d+").unwrap();
+    static ref ISSUE: Regex = Regex::new(r"#?(\d+)").unwrap();
     static ref REPOISSUE: Regex = Regex::new(r"[\w-]+[#/]\d+").unwrap();
     static ref OWNERREPOISSUE: Regex = Regex::new(r"([\w-]+)/([\w-]+)[#/](\d+)").unwrap();
     static ref URL: Regex = Regex::new(r"https?://github.com/[\w-]+/issues/[\w-]+[#/]\d+").unwrap();
     static ref BASE: Url = Url::parse("https://api.github.com/repos/").unwrap();
-    static ref REMOTE: Regex = Regex::new(r"(?:https://github.com/([\w-]+)/([\w-]+).git)|(?:git@github.com:([\w-]+)/([\w-]+).git)").unwrap();
+    static ref REMOTE: Regex = Regex::new(
+        r"(?:https://github.com/([\w-]+)/([\w-]+).git)|(?:git@github.com:([\w-]+)/([\w-]+).git)"
+    )
+    .unwrap();
 }
 
 /// Data returned from the Github issue API
@@ -26,17 +29,18 @@ lazy_static! {
 /// Currently we only care about the state (open/closed)
 // TODO: Add the date it was closed here?
 #[derive(Deserialize)]
-struct GithubIssue {
-    state: String
+#[serde(untagged)]
+enum GithubIssueResponse {
+    Ok { state: String },
+    Err { message: String },
 }
-
 
 #[proc_macro]
 pub fn blocked(input: TokenStream) -> TokenStream {
     // Parse Arguments
     let (issue_pattern, reason) = match parse_args(input) {
         Ok(args) => args,
-        Err(err) => return TokenStream::from(err.to_compile_error())
+        Err(err) => return TokenStream::from(err.to_compile_error()),
     };
 
     // Check if we have an API key, otherwise exit silently
@@ -51,19 +55,32 @@ pub fn blocked(input: TokenStream) -> TokenStream {
     // Try to resolve the issue pattern to an issue API URL
     let url = match parse_issue_pattern(&issue_pattern) {
         Ok(url) => url,
-        Err(err) => return TokenStream::from(err.to_compile_error())
+        Err(err) => return TokenStream::from(err.to_compile_error()),
     };
     eprintln!("{}", url);
 
     // Get issue status
     let r = client.get(url).send().unwrap();
-    let issue = r.json::<GithubIssue>().unwrap();
+    let issue = r.json::<GithubIssueResponse>().unwrap();
+    let issue_state = match issue {
+        GithubIssueResponse::Err { message } => {
+            return TokenStream::from(
+                error(format!("Error fetching issue: {}", message)).to_compile_error(),
+            )
+        }
+        GithubIssueResponse::Ok { state } => state,
+    };
 
     // Warn if the issue has been closed
-    match issue.state.as_str() {
+    match issue_state.as_str() {
         "open" => (),
-        "closed" => Diagnostic::spanned([Span::call_site()].as_ref(), Level::Note, reason.unwrap_or_else(|| "Issue was closed.".to_string())).emit(),
-        _ => panic!("unknown response")
+        "closed" => Diagnostic::spanned(
+            [Span::call_site()].as_ref(),
+            Level::Warning,
+            reason.unwrap_or_else(|| "Issue was closed.".to_string()),
+        )
+        .emit(),
+        _ => panic!("unknown response"),
     }
 
     TokenStream::new()
@@ -77,40 +94,73 @@ fn parse_args(input: TokenStream) -> Result<(String, Option<String>), syn::Error
         return Err(error("Expected between 1 and 2 arguments"));
     }
     let mut args_iter = args.iter();
-    Ok(
-        (
-            args_iter.next().ok_or_else(|| error("Expected an issue pattern as a first argument."))?.value(),
-            args_iter.next().map(|s| s.value())
-        )
-    )
+    Ok((
+        args_iter
+            .next()
+            .ok_or_else(|| error("Expected an issue pattern as a first argument."))?
+            .value(),
+        args_iter.next().map(|s| s.value()),
+    ))
 }
 
 /// Get a client suitable for interacting with the Github API
 fn github_client(api_key: &str) -> reqwest::blocking::Client {
     let mut headers = HeaderMap::new();
-    headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(api_key).unwrap());
-    headers.insert(header::USER_AGENT, header::HeaderValue::from_static("blocked-rs"));
-    reqwest::blocking::Client::builder().default_headers(headers).build().unwrap()
+    headers.insert(
+        header::AUTHORIZATION,
+        header::HeaderValue::from_str(api_key).unwrap(),
+    );
+    headers.insert(
+        header::USER_AGENT,
+        header::HeaderValue::from_static("blocked-rs"),
+    );
+    reqwest::blocking::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap()
 }
 
 /// Parse an issue pattern. Possible forms are documented on the main `blocked!` macro
 fn parse_issue_pattern(pattern: &str) -> Result<Url, syn::Error> {
     if URL.is_match(pattern) {
-        return Url::parse(pattern).map_err(|_| error("URL matched regex but was not accepted by the URL crate"))
+        return Url::parse(pattern)
+            .map_err(|_| error("URL matched regex but was not accepted by the URL crate"));
     }
     if let Some(captures) = OWNERREPOISSUE.captures(pattern) {
-        return BASE.clone()
-            .join(&format!("{}/{}/issues/{}", captures.get(1).unwrap().as_str(), captures.get(2).unwrap().as_str(), captures.get(3).unwrap().as_str())).map_err(|_| error("Could not join URL fragments"))
+        return BASE
+            .clone()
+            .join(&format!(
+                "{}/{}/issues/{}",
+                captures.get(1).unwrap().as_str(),
+                captures.get(2).unwrap().as_str(),
+                captures.get(3).unwrap().as_str()
+            ))
+            .map_err(|_| error("Could not join URL fragments"));
     }
     if let Some(captures) = REPOISSUE.captures(pattern) {
         let (org, _) = try_get_org_repo()?;
-        return BASE.clone()
-            .join(&format!("{}/{}/issues/{}", org, captures.get(1).unwrap().as_str(), captures.get(2).unwrap().as_str())).map_err(|_| error("Could not join URL fragments"))
+        return BASE
+            .clone()
+            .join(&format!(
+                "{}/{}/issues/{}",
+                org,
+                captures.get(1).unwrap().as_str(),
+                captures.get(2).unwrap().as_str()
+            ))
+            .map_err(|_| error("Could not join URL fragments"));
     }
     if let Some(captures) = ISSUE.captures(pattern) {
         let (org, repo) = try_get_org_repo()?;
-        return BASE.clone()
-            .join(&format!("{}/{}/issues/{}", org, repo, captures.get(2).unwrap().as_str())).map_err(|_| error("Could not join URL fragments"))
+        eprintln!("got repo");
+        return BASE
+            .clone()
+            .join(&format!(
+                "{}/{}/issues/{}",
+                org,
+                repo,
+                captures.get(1).unwrap().as_str()
+            ))
+            .map_err(|_| error("Could not join URL fragments"));
     }
     Err(error("Could not parse issue pattern"))
 }
@@ -119,16 +169,36 @@ fn parse_issue_pattern(pattern: &str) -> Result<Url, syn::Error> {
 ///
 /// This is used for shorthand issue patterns.
 fn try_get_org_repo() -> Result<(String, String), syn::Error> {
-    let repo = Repository::open_from_env().map_err(|_| error("Could not find or open a git repository"))?;
+    let repo = Repository::open_from_env()
+        .map_err(|_| error("Could not find or open a git repository"))?;
 
     let remote = if let Ok(remote) = repo.find_remote("upstream") {
         Some(remote)
     } else {
         repo.find_remote("origin").ok()
-    }.ok_or_else(|| error("Could not find an 'upstream' or 'origin' remote"))?;
+    }
+    .ok_or_else(|| error("Could not find an 'upstream' or 'origin' remote"))?;
 
-    REMOTE.captures(remote.url().ok_or_else(|| error("Remote URL not valid unicode"))?)
-        .map(|captures| (captures.get(1).unwrap().as_str().to_owned(), captures.get(2).unwrap().as_str().to_owned()))
+    REMOTE
+        .captures(
+            remote
+                .url()
+                .ok_or_else(|| error("Remote URL not valid unicode"))?,
+        )
+        .map(|captures| {
+            (
+                captures
+                    .get(1)
+                    .unwrap_or_else(|| captures.get(3).unwrap())
+                    .as_str()
+                    .to_owned(),
+                captures
+                    .get(2)
+                    .unwrap_or_else(|| captures.get(4).unwrap())
+                    .as_str()
+                    .to_owned(),
+            )
+        })
         .ok_or_else(|| error("Failed to parse remote URL"))
 }
 
